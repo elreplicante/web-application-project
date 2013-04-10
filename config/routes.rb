160f7f7664cfa8e808ea431d317241fe84047fa1
Sergio Revilla <sergio@unmundotecnologico.org>
2013-04-10 14:18:27 +0200

    created users show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WebApplicationProject::Application.routes.draw do
-  get "users/new"
+  resources :users
+  
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
@@ -7,6 +8,8 @@ WebApplicationProject::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   match '/signup',  to: 'users#new'
+
+
 
 
 
