a3ec3ba3c25996184b56e3363a48b8eb1398eb3a
Sergio Revilla <sergio@unmundotecnologico.org>
2013-04-11 14:06:30 +0200

    fixed sign out bug

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ WebApplicationProject::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
-  match '/signout',  to: 'sessios#destroy', via: :delete
+  match '/signout',  to: 'sessions#destroy', via: :delete
 
 
 
