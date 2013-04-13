52300d0f47e6ba74d16f5abfa55b66c420254f14
Sergio Revilla <sergio@unmundotecnologico.org>
2013-04-13 13:06:17 +0200

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user,  only:[:edit, :update]
+  before_filter :correct_user,    only:[:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -31,5 +34,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?  
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+    
   end
 end
