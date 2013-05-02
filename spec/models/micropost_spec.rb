9a51c57deb14692cdf2556e0656bbf8c154f719d
Sergio Revilla <sergio@unmundotecnologico.org>
2013-05-02 14:01:26 +0200

    micro post resource finished

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -14,11 +14,6 @@ describe Micropost do
 
 	it { should be_valid}
 
-	describe "when user id is not present" do
-		before { @micropost.user_id = nil }
-		it { should_not be_valid }
-	end
-
 	describe "accessible attributes" do
 		it "should not allow access to user_id" do
 			expect do
@@ -26,4 +21,19 @@ describe Micropost do
 			end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
 		end
 	end
+
+	describe "when user id is not present" do
+		before { @micropost.user_id = nil }
+		it { should_not be_valid }
+	end
+
+	describe "with blank content" do
+    before { @micropost.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
+    it { should_not be_valid }
+  end
 end
