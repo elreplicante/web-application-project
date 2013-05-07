a6bb63123b548f03752f02cdd3bc303525f6d667
Sergio Revilla <sergio@unmundotecnologico.org>
2013-05-07 21:03:14 +0200

    added transitions on page load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require bootstrap
 //= require_tree .
 
+$(document).ready(function() {
+    $("body").css("display", "none");
+ 
+    $("body").fadeIn(1000);
+ 
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);      
+    });
+
+    $('.tooltip').tooltip();
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+});
+
